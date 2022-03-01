81ce00d97fe9ae82ed99d1c68001de4ccae8e5ec
singularitea <geomotion-ben@DESKTOP-5SSTMML.localdomain>
2022-03-01 14:53:43 +0800

    boards created WIP

--- day-04/main.go
+++ day-04/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -16,9 +17,13 @@ func check(e error) {
 	}
 }
 
-type board struct {
+type number struct {
 	number int
 	seen   bool
+}
+
+type board struct {
+	board [][]number
 }
 
 func splitText(word string) []string {
@@ -44,118 +49,57 @@ func main() {
 
 	// boards := make([][][]string, len(boardsRaw))
 
-	dat, err = os.ReadFile("test.txt")
+	dat, err = os.ReadFile("boards.txt")
 	check(err)
-	test := strings.Split(string(dat), "\n")
+	test := strings.Split(string(dat), "\n\n")
 
-	boards := make([][]strings, len())
+	// boards := make([][]string, len(test))
+	var boards []board
 
-
+	// newString2 := make([][]string, len(test))
 	for i := range test {
-		newString := ""
-		newString = strings.Replace(test[i], " ", "", -1)
-		newString = strings.Replace(newString, " ", "", -1)
-		newString = strings.Replace(newString, "\n", "", -1)
-		for j := range 
-		test[i] == make(newString
+		var board board
+		newString := test[i]
+		newString = strings.Replace(newString, "  ", " ", -1)
+		// newString = strings.Replace(newString, "\r ", "\r", -1)
+		newString = strings.Replace(newString, "\n ", "\n", -1)
+		newString = strings.Replace(newString, " \n", "\n", -1)
+		newString = strings.Replace(newString, "\n", " ", -1)
+		if string(newString[0]) == " " {
+			newString = strings.Replace(newString, " ", "", 1)
+		}
 
+		tempBoard := strings.Split(newString, " ")
+		// for l := range tempBoard {
+		// 	fmt.Println(string(tempBoard[l]))
+		// }
+		// os.Exit(1)
+		count := 0
+		var row []number
+		for j := range tempBoard {
+			// fmt.Println(tempBoard[j])
+			tempNum, err := strconv.Atoi(tempBoard[j])
+			check(err)
+			n := number{number: tempNum, seen: false}
+			if count < 5 {
+				row = append(row, n)
+				count++
+
+			} else {
+				count = 0
+				board.board = append(board.board, row)
+				row = nil
+			}
+			// fmt.Println(board.board)
+
+			// newString = strings.Replace(newString, " ", "", -1)
+
+			// append(newString, newString2)
+			// }
+
+			boards = append(boards, board)
+		}
 	}
-	for i := range test {
-		fmt.Println(test[i])
-	}
-
-	// for i := range test {
-	// 	test2 := strings.Split(string(dat), " ")
-	// 	print(i)
-	// 	for j := range test2 {
-	// 		fmt.Println(test2[j])
-	// 	}
-	// }
-
-	// i :=0
-	// for i < 5{
-	// 	append(boards[i], board)
-	// }
-
-	// newBoards := make([][]string, len(boards))
-	// fmt.Println(newBoards)
-
-	// for i := range boards {
-	// 	nb := strings.Split(string(boards[i]), " ")
-	// 	append(newBoards[i], nb)
-	// }
-
-	// // using custom type for map
-	// count := make([]M, len(a[0]))
-
-	// // create the map for storing the count
-	// for i := range count {
-	// 	m1 := M{"0": 0, "1": 0}
-	// 	count[i] = m1
-	// }
-
-	// for i := range a {
-	// 	for j := range a[i] {
-	// 		switch a[i][j : j+1] {
-	// 		case "0":
-	// 			count[j]["0"] = count[j]["0"] + 1
-	// 		case "1":
-	// 			count[j]["1"] = count[j]["1"] + 1
-	// 		}
-	// 	}
-	// }
-
-	// gammaStr := ""
-	// epsilonStr := ""
-
-	// for i := range count {
-	// 	if count[i]["1"] > count[i]["0"] {
-	// 		gammaStr = gammaStr + "1"
-	// 		epsilonStr = epsilonStr + "0"
-	// 	} else {
-	// 		gammaStr = gammaStr + "0"
-	// 		epsilonStr = epsilonStr + "1"
-	// 	}
-	// }
-
-	// println("gamma:", gammaStr)
-	// println("epsilon:", epsilonStr)
-
-	// gamma, err := strconv.ParseInt(gammaStr, 2, 0)
-	// check(err)
-
-	// epsilon, err := strconv.ParseInt(epsilonStr, 2, 0)
-	// check(err)
-
-	// println("consumption:", gamma*epsilon)
-
-	// // part two
-
-	// fmt.Printf("%v", count)
-
-	// ox := make([]string, len(a))
-	// copy(ox, a)
-
-	// c02 := make([]string, len(a))
-	// copy(c02, a)
-
-	// for j := range a[0] {
-	// 	for i := range ox {
-	// 		fmt.Println(len(ox))
-	// 		if len(ox) == 1 {
-	// 			break
-	// 		} else if count[j]["0"] > count[j]["1"] {
-	// 			if a[i][j] != 0 {
-	// 				ox = remove(ox, i)
-	// 			}
-	// 		} else {
-	// 			if a[i][j] != 1 {
-	// 				ox = remove(ox, i)
-	// 			}
-	// 		}
-
-	// 	}
-	// }
-	// fmt.Println(ox)
+	fmt.Println(boards)
 
 }
