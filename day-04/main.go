4e65ebf555ccca45343345d4e198774791050818
singularitea <geomotion-ben@DESKTOP-5SSTMML.localdomain>
2022-03-02 15:55:27 +0800

    checking board WIP

--- day-04/main.go
+++ day-04/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -17,6 +16,44 @@ func check(e error) {
 	}
 }
 
+func checkBoardHasWon(board board) int {
+	for i := range board.board {
+		countHorizontal := 0
+		totalHorizontal := 0
+		horizontal := false
+		for l := range board.board[i] {
+			if board.board[i][l].seen == true {
+				countHorizontal++
+				totalHorizontal += board.board[i][l].number
+			}
+			if countHorizontal == 5 {
+				horizontal = true
+			}
+		}
+
+		countVertical := 0
+		totalVertical := 0
+		vertical := false
+		for l := range board.board[i] {
+			for j := range board.board {
+				if board.board[i][l].seen == true {
+					countVertical++
+					totalVertical += board.board[i][l].number
+				}
+				if countVertical == 5 {
+					vertical = true
+				}
+			}
+		}
+		if vertical == true || horizontal == true {
+			return totalHorizontal + totalVertical
+		} else {
+			return 0
+		}
+	}
+	return 0
+}
+
 type number struct {
 	number int
 	seen   bool
@@ -24,15 +61,6 @@ type number struct {
 
 type board struct {
 	board [][]number
-}
-
-func splitText(word string) []string {
-	array := regexp.MustCompile("[\\ \\,\\\n\\s]+").Split(word, -1)
-	return array
-}
-
-func remove(s []string, i int) []string {
-	return append(s[:i], s[i+1:]...)
 }
 
 func main() {
@@ -58,7 +86,6 @@ func main() {
 
 	// newString2 := make([][]string, len(test))
 	for i := range test {
-		var board board
 		newString := test[i]
 		newString = strings.Replace(newString, "  ", " ", -1)
 		// newString = strings.Replace(newString, "\r ", "\r", -1)
@@ -73,9 +100,11 @@ func main() {
 		// for l := range tempBoard {
 		// 	fmt.Println(string(tempBoard[l]))
 		// }
+		// fmt.Println("hi")
 		// os.Exit(1)
 		count := 0
 		var row []number
+		var board board
 		for j := range tempBoard {
 			// fmt.Println(tempBoard[j])
 			tempNum, err := strconv.Atoi(tempBoard[j])
@@ -96,10 +125,31 @@ func main() {
 
 			// append(newString, newString2)
 			// }
+		}
+		// fmt.Println("board", board)
+		boards = append(boards, board)
+	}
 
-			boards = append(boards, board)
+	for i := range numbers {
+		for j := range boards {
+			for k := range boards[j].board {
+				for l := range boards[j].board[k] {
+					num, err := strconv.Atoi(numbers[i])
+					check(err)
+					if boards[j].board[k][l].number == num {
+						boards[j].board[k][l].seen = true
+						checkBoardHasWon(boards[j])
+					}
+				}
+				// fmt.Println(i)
+				// fmt.Println(boards[j].board[k])
+			}
 		}
 	}
+
+	// response := fmt.Sprintf("board %d", j)
+	// fmt.Println(response)
+	// fmt.Println(boards[j].board)
 	fmt.Println(boards)
 
 }
